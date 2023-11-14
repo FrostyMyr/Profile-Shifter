c55e9ef93f40c23583767478852422af01890005
Rama <55007364+Arararra@users.noreply.github.com>
2023-11-14 18:41:37 +0700

    check webhook profile (blurry)

--- index.js
+++ index.js
@@ -118,15 +118,27 @@ client.on("messageCreate", async (message) => {
 });
 
 // Add reaction command
-client.on('messageReactionAdd', async (reaction) => {
-  if (reaction.emoji.name != '🗑️') return;
+client.on('messageReactionAdd', async (reaction, user) => {
+  if (reaction.emoji.name != '🗑️' && reaction.emoji.name != '🔍') return;
   
   const messageId = reaction.message.id;
   const channelId = reaction.message.channelId;
   const message = await client.channels.cache.get(channelId).messages.fetch(messageId);
 
   if (message.applicationId == client.user.id) {
-    await message.delete();
+    if (reaction.emoji.name == '🗑️') {
+      await message.delete();
+    } else if (reaction.emoji.name == '🔍') {
+      reaction.users.remove(user.id);
+      const avatarUrl = `https://cdn.discordapp.com/avatars/${message.author.id}/${message.author.avatar}.png`;
+      user.send({
+        content: `${message.author.username}`,
+        files: [{ 
+          name: `${message.author.username}_avatar.png`, 
+          attachment: avatarUrl
+        }]
+      });
+    }
   }
 });
 
