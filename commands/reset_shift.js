6276307ce749d586222b14acb76880144db9c384
Rama <55007364+Arararra@users.noreply.github.com>
2023-11-18 12:53:14 +0700

    fix reset shift

--- commands/reset_shift.js
+++ commands/reset_shift.js
@@ -7,7 +7,30 @@ module.exports = {
     .setName("reset_shift")
     .setDescription("Reset profile shift."),
   async execute(interaction, client) {
+    const user = interaction.user;
+
+    if (!user.bot && !interaction.member.roles.cache.some(x => x.name == 'Assigner')) {
+      interaction.reply({
+        content: `You don't have the role for that`,
+        ephemeral: true
+      });
+      return;
+    }
+
+    const members = await client.guilds.cache.get(interaction.guild.id).members.fetch().then(member => {
+      return Array.from(member).filter(x => !x[1].user.bot).map(x => x[0]);
+    });
+
     fs.writeFileSync(`./${interaction.guild.id}_profile_shift.json`, '{}');
+    const profileShiftJson = JSON.parse(fs.readFileSync(`./${interaction.guild.id}_profile_shift.json`));
+
+    members.forEach((memberId) => {
+      profileShiftJson[memberId] = { 
+        "id": memberId
+      };
+    });
+
+    fs.writeFileSync(`./${interaction.guild.id}_profile_shift.json`, JSON.stringify(profileShiftJson, null, 2));
 
     interaction.reply({
       content: `Profile shift is resetted`,
